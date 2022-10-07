2034bfc49a945fbbadfd9d01d6cafab70bf7a751
agardnerit <adam@agardner.net>
2022-10-07 21:26:33 +1000

    update

--- plugins/netlify-plugin-hello-world/index.js
+++ plugins/netlify-plugin-hello-world/index.js
@@ -1,5 +1,6 @@
 // https://docs.netlify.com/integrations/build-plugins/create-plugins
 
+/*
 module.exports = {
   onPreBuild: ({ constants, netlifyConfig, packageJson}) => {
     console.log("Hello world from onPreBuild event!");
@@ -15,23 +16,45 @@ module.exports = {
     console.log('---------------');
   },
 }
+*/
+
+const { env } = require('process')
 
 module.exports = {
-  onPostBuild: ({ constants, netlifyConfig, packageJson}) => {
+  onPostBuild: ({ constants, netlifyConfig, utils}) => {
+
     console.log("Hello world from onPostBuild event!");
-    //console.log('---------------');
-    //console.log('Printing constants');
-    //console.log(constants);
-    console.log('---------------');
-    console.log('Printing netlifyConfig');
+    console.log(constants);
+    console.log("--------------------------");
     console.log(netlifyConfig);
-    console.log('---------------');
-    console.log('Printing packageJson')
-    console.log(packageJson);
-    console.log('---------------');
-  },
+
+    const { git } = utils;
+
+    /* Do stuff if files modified */
+    if (git.modifiedFiles.length !== 0) {
+      console.log('Modified files:', git.modifiedFiles);
+    }
+
+    /* Do stuff only if html code edited */
+    const htmlFiles = git.fileMatch('**/*.html');
+    console.log('html files git info:', htmlFiles);
+
+    if (htmlFiles.edited.length !== 0) {
+      console.log('>> Run thing because HTML has changed\n');
+    }
+    
+    /* Do stuff only if markdown files edited */
+    const markdownFiles = git.fileMatch('**/*.md');
+    console.log('markdown files git info:', markdownFiles);
+
+    if (markdownFiles.modified.length !== 0) {
+      console.log('>> Run thing because Markdown files have been created/changed/deleted\n');
+    }
+
+  }
 }
 
+/*
 module.exports = {
   onSuccess: ({ constants, netlifyConfig, packageJson}) => {
     console.log("Hello world from onSuccess event!");
@@ -47,3 +70,4 @@ module.exports = {
     console.log('---------------');
   },
 }
+*/

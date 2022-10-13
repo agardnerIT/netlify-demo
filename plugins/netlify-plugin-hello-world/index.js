dac6ec1db5db46f8a3ae20876b85a70f676b4d0a
agardnerit <adam@agardner.net>
2022-10-13 15:12:18 +1000

    1

--- plugins/netlify-plugin-hello-world/index.js
+++ plugins/netlify-plugin-hello-world/index.js
@@ -22,16 +22,34 @@ const { env } = require('process')
 
 var fs = require('fs');
 
-
+/*
+  This plugin is designed to be used wherever a sitemap.xml is available
+  1) It will create a .dynatrace folder in the repo
+  2) It will create .dynatrace/config.json and populate it
+  3) It will grab sitemap.xml from the baseUrl+"/sitemap.xml" and place it in the .dynatrace folder
+*/
 module.exports = {
-  onPostBuild: ({ constants, netlifyConfig, utils}) => {
+  onSuccess: ({ constants, netlifyConfig, utils}) => {
 
-    console.log("Hello world from onPostBuild event!");
+    const { run } = utils
+
+    let baseUrl = process.env.DEPLOY_PRIME_URL; // Netlify deploy preview or base URL
+    let sitemapUrl = baseUrl+"/sitemap.xml"
+    run.command("ls -l")
+    console.log("Base URL: " + baseUrl)
+    console.log("----")
+    console.log(ls -l)
+
+    // Old logic below
+    /*
+
+    console.log("Hello world from onSuccess event!");
+    console.log(process.env)
 
     let baseUrl = process.env.DEPLOY_PRIME_URL; // Netlify deploy preview or base URL
   
-    let dt_url = process.env.dt_environment_url; // https://abc12345.live.dynatrace.com
-    let dt_api_token = process.env.dt_api_token; // dtc01.sample.secret
+    let dt_url = process.env.DT_ENVIRONMENT_URL; // https://abc12345.live.dynatrace.com
+    let dt_api_token = process.env.FOO; // dtc01.sample.secret
     console.log("DT URL: " + dt_url);
     console.log("DT API TOKEN: " + dt_api_token);
 
@@ -43,51 +61,40 @@ module.exports = {
     let rawdata = fs.readFileSync(dt_pages_file);
     let pages_file = JSON.parse(rawdata);
 
-    // Create one-off synthetic checks for each page
-    pages_file.forEach((element, index, array) =>
-        console.log(baseUrl + element['path'] + " must respond in: " + element['time'])
-    )
+    /* Create one-off synthetic checks for each page
+     * Step 1: For each URL, see if an HTTP_CHECK already exists  (entities.read)
+     * curl -X GET "https://jao16384.sprint.dynatracelabs.com/api/v2/entities?entitySelector=type%28HTTP_CHECK%29%2Ctag%28netlify%29" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token dtc01.***.***"
+     *
+    console.log("Fetching from ${dt_url}");
+
+    const response = fetch(dt_url + "/api/v2/entities?entitySelector=type(HTTP_CHECK),tag(netlify)",
+        {
+          method: "GET",
+          headers: {
+            "Accept": "application/json; charset=utf-8",
+            "Authorization": "Api-Token " + dt_api_token
+          }
+        });
+    const data = response.json();
+    console.log("Printing data from DT API GET:");
+    console.log(data);
+
+    /*
+     * Step 2: Create an HTTP_CHECK for each URL
+     * Step 2: Trigger that check
+     * Step 3: Retrieve evaluations when done
+     *
+    pages_file.forEach((element, index, array) => {
+        full_url = dt_url + element['path'];
+        console.log(full_url + " must respond in: " + element['time'])
+    })
 
     const { git } = utils;
 
-    /* Do stuff if files modified */
+    // Do stuff if files modified
     if (git.modifiedFiles.length !== 0) {
       console.log('Modified files:', git.modifiedFiles);
     }
-
-    /* Do stuff only if html code edited */
-    const htmlFiles = git.fileMatch('**/*.html');
-    console.log('html files git info:', htmlFiles);
-
-    if (htmlFiles.edited.length !== 0) {
-      console.log('>> Run thing because HTML has changed\n');
-    }
-    
-    /* Do stuff only if markdown files edited */
-    const markdownFiles = git.fileMatch('**/*.md');
-    console.log('markdown files git info:', markdownFiles);
-
-    if (markdownFiles.modified.length !== 0) {
-      console.log('>> Run thing because Markdown files have been created/changed/deleted\n');
-    }
-
+    */
   }
 }
-
-/*
-module.exports = {
-  onSuccess: ({ constants, netlifyConfig, packageJson}) => {
-    console.log("Hello world from onSuccess event!");
-    //console.log('---------------');
-    //console.log('Printing constants');
-    //console.log(constants);
-    console.log('---------------');
-    console.log('Printing netlifyConfig');
-    console.log(netlifyConfig);
-    console.log('---------------');
-    console.log('Printing packageJson')
-    console.log(packageJson);
-    console.log('---------------');
-  },
-}
-*/
